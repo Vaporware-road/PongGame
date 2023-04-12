fe7e955248265e133934a3635b9bdd82c0e760d5
Abyys001 <siavash0darvishi@gmail.com>
2023-04-12 12:49:01 +0430

    Creating ball update

--- TestingPygame.py
+++ TestingPygame.py
@@ -54,7 +54,18 @@ class Paddle:
 
 
 class Ball:
-    pass
+    MAX_VEL = 5
+    COLOR = WHITE
+
+    def __init__(self, x, y, radius):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.x_vel = self.MAX_VEL
+        self.y_vel = 0
+
+    def draw(self, win):
+        pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.radius)
 
 
 def handle_paddle_movement(keys, left_paddle, right_paddle):
