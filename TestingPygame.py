5d3aaf8bc73a32365cfcddf1f06784899e5b666b
Abyys001 <siavash0darvishi@gmail.com>
2023-04-13 09:24:13 +0430

    Window Update

--- TestingPygame.py
+++ TestingPygame.py
@@ -9,8 +9,8 @@ import pygame
 
 pygame.init()
 
-HEIGHT, WIDTH = 900, 600
-WN = pygame.display.set_mode((HEIGHT, WIDTH))
+WIDTH, HEIGHT = 900, 600
+WN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pong by V.road")
 
 WHITE = (255, 255, 255)
@@ -26,10 +26,10 @@ def draw(win, paddles, ball):
     for paddle in paddles:
         paddle.draw(win)
 
-    for i in range(10, HEIGHT, HEIGHT // 20):
+    for i in range(10, HEIGHT, HEIGHT // 10):
         if i % 2 == 1:
             continue
-        pygame.draw.rect(win, WHITE, (WIDTH - 160, i, 10, HEIGHT // 20))
+        pygame.draw.rect(win, WHITE, (WIDTH / 2, i, 10, HEIGHT // 20))
 
     ball.draw(win)
 
@@ -57,7 +57,7 @@ class Paddle:
 
 
 class Ball:
-    MAX_VEL = 5
+    MAX_VEL = 4.5
     COLOR = WHITE
 
     def __init__(self, x, y, radius):
@@ -73,6 +73,35 @@ class Ball:
     def move(self):
         self.x += self.x_vel
         self.y += self.y_vel
+
+
+def handle_collision(ball, left_paddle, right_paddle):
+    if ball.y + ball.radius >= HEIGHT:
+        ball.x_vel *= -1
+    elif ball.y - ball.radius <= 0:
+        ball.y_vel *= -1
+
+    if ball.x_vel < 0:  # left paddle
+        if left_paddle.y <= ball.y <= left_paddle.y + left_paddle.height:
+            if ball.x - ball.radius <= left_paddle.x + left_paddle.width:
+                ball.x_vel *= -1
+
+                middle_y = (left_paddle.y + left_paddle.height)
+                difference_in_y = middle_y - ball.y
+                reduction_factor = (left_paddle.height // 2) / ball.MAX_VEL
+                y_vel = difference_in_y // reduction_factor
+                ball.y_vel = -1 * y_vel
+
+    else:  # right paddle
+        if right_paddle.y <= ball.y <= right_paddle.y + right_paddle.height:
+            if ball.x + ball.radius >= right_paddle.x:
+                ball.x_vel *= -1
+
+                middle_y = (right_paddle.y + right_paddle.width)
+                difference_in_y = middle_y - ball.y
+                reduction_factor = (right_paddle.height // 2) // ball.MAX_VEL
+                y_vel = difference_in_y // reduction_factor
+                ball.y_vel = -1 * y_vel
 
 
 def handle_paddle_movement(keys, left_paddle, right_paddle):
@@ -91,10 +120,10 @@ def main():
     run_game = True
     clock = pygame.time.Clock()
 
-    left_paddle = Paddle(10, HEIGHT // 4, PADDLE_WIDTH, PADDLE_HEIGHT)
-    right_paddle = Paddle(WIDTH + WIDTH // 4 + WIDTH // 5 + 5, HEIGHT // 4, PADDLE_WIDTH, PADDLE_HEIGHT)
+    left_paddle = Paddle(10, (HEIGHT - PADDLE_HEIGHT) // 2, PADDLE_WIDTH, PADDLE_HEIGHT)
+    right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDTH, (HEIGHT - PADDLE_HEIGHT) // 2, PADDLE_WIDTH, PADDLE_HEIGHT)
 
-    ball = Ball(WIDTH // 1.35, HEIGHT // 3.2, BALL_RADIUS)
+    ball = Ball(WIDTH // 2, HEIGHT // 2, BALL_RADIUS)
 
     while run_game:
         clock.tick(60)
@@ -104,12 +133,13 @@ def main():
         keys = pygame.key.get_pressed()
         handle_paddle_movement(keys, left_paddle, right_paddle)
 
+        handle_collision(ball, left_paddle, right_paddle)
+        ball.move()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run_game = False
                 break
-            
-        ball.move()
 
     pygame.quit()
 
